3a49a922eca41bd85fc6be8ec6af848f40c5d5ce
edzefcp <edzel_franz@yahoo.com>
2018-03-13 19:35:37 +0400

    Integrated Tabbed Layout

---
+++ app/src/main/java/com/example/samsung/UniTeach/PagerAdapter.java
@@ -0,0 +1,41 @@
+package com.example.samsung.UniTeach;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter{
+
+    int mNoOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumberOfTabs){
+        super(fm);
+        this.mNoOfTabs = NumberOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position)
+        {
+            case 0:
+                HomeFragment tab1 = new HomeFragment();
+                return tab1;
+
+            case 1:
+                TutorFragment tab2 = new TutorFragment();
+                return tab2;
+
+            case 2:
+                BookSellingFragment tab3 = new BookSellingFragment();
+                return tab3;
+
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNoOfTabs;
+    }
+}
